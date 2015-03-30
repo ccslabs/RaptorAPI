bccba0de38057f5a1929dd9498103aff5195492a
ccslabs <ccslabs@hotmail.com>
2015-03-30 18:25:15 +0100

    Initial Commit - Hello Command

---
+++ RaptorTCPAPI4/IRaptorAPI4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace RaptorTCPAPI4
+{
+   
+    [ServiceContract]
+    public interface IRaptorAPI4
+    {
+        bool Hello();       // Simply returns true if called. Quick test to see if the service is online
+
+       
+    }
+
+    
+}
---
+++ RaptorTCPAPI4/RaptorAPI4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace RaptorTCPAPI4
+{
+    
+    public class RaptorAPI4 : IRaptorAPI4
+    {
+        // Simple test to see if the server is online
+        public bool Hello()
+        {
+            return true;
+        }
+    }
+}
